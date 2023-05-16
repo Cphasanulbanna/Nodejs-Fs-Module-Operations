f76a722eb236478db83ac17b3fb9e559f91cf17e
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-05-16 11:26:52 +0530

    completed math operations

---
+++ index.js
@@ -0,0 +1,5 @@
+const { add, subtract, divide } = require("./utils");
+
+console.log("result of addition:", add(10, 20));
+console.log("result of subtraction:", subtract(100, 50));
+console.log("result of division:", divide(10, 10));
---
+++ utils.js
@@ -0,0 +1,16 @@
+//Addition
+const add = (number1, number2) => {
+    return number1 + number2;
+};
+
+//subtraction
+const subtract = (number1, number2) => {
+    return number1 - number2;
+};
+
+//division
+const divide = (number1, number2) => {
+    return number1 / number2;
+};
+
+module.exports = { add, subtract, divide };
